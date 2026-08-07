19f6f62514c94381a42e6f8797d492698370f84c
veeloflash <mingrui067@gmail.com>
2026-08-07 17:35:28 +0200

    Delete week21-master directory

--- week21-master/Week21_Engineering/Implementation5/Embedding_Search/embedding_search.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from Week21_Engineering.shared.shared import documents, document_vectors, encode_texts
-
-def search2(query, k=5):
-    qv = encode_texts(query)[0]
-    sims = np.dot(document_vectors, qv)
-    idx = np.argsort(sims)[::-1][:k]
-    return [(documents[i], float(sims[i])) for i in idx]
